9e4ceb66908539cc88160ab3de51666357be88f8
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2024-03-03 11:22:40 +0000

    using ajax method

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -17,9 +17,50 @@ $(document).ready(function () {
     }
   });
 
-  $.ajax('http://0.0.0.0:5001/api/v1/places_search/', {
-    type: 'POST',
-    data: ,
-    contentType:
-  })
+$.ajax({
+  url: "http://0.0.0.0:5001/api/v1/places_search/",
+  type: "POST",
+  contentType: "application/json, charset=utf-8",
+  dataType: "json",
+  data: "{}",
+  success: (response) => {
+    for (let place of response) {
+      $("section.place").append(
+        `
+            <article>
+            <div class="title_box">
+              <h2>` +
+          place.name +
+          `</h2>
+            <div class="price_by_night">` +
+          place.price_by_night +
+          `</div>
+            </div>
+            <div class="information">
+              <div class="max_guest">` +
+          place.max_guest +
+          (place.max_guest !== 1 ? " Guests" : " Guest") +
+          `</div>
+                <div class="number_rooms">` +
+          place.number_rooms +
+          (place.number_rooms !== 1 ? " Bedrooms" : " Bedroom") +
+          `</div>
+                <div class="number_bathrooms">` +
+          place.number_bathrooms +
+          (place.number_bathrooms !== 1 ? " Bathrooms" : " Bathroom") +
+          `</div>
+            </div>
+            <div class="user">
+              <b>Owner:</b> first name last name
+            </div>
+            <div class="description">` +
+          place.description +
+          `</div>
+            </article>
+            `
+      );
+    }
+  },
+});
+
 });
